7c8dd18cd97dbde07fec86478e639c9e22890e0e
dharmveer <dharmveer.baloda@rubique.com>
2018-01-03 20:44:24 +0530

    allowed origin removed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(AllowedOrigins());
+//app.use(AllowedOrigins());
 app.use('/', index);
 
 // catch 404 and forward to error handler
